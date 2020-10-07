3b823d55baa62496f9f21bb7994522c53de7a4c8
Mark OBrien <obrienmark@johndeere.com>
2020-10-07 11:54:02 -0500

    Finished automatically figuring winnter

--- js/script.js
+++ js/script.js
@@ -6,22 +6,14 @@ let colors = ["red","blue"];
 let whoseTurn = 0;
 let redScore = 0;
 let blueScore = 0;
+let clicks = 0; //used to track how many boxes clicked
 
 //find dom objects
 let redScoreElem = document.getElementById("redScore");
 let blueScoreElem = document.getElementById("blueScore");
 let whoseTurnElem = document.getElementById("whoseTurn");
+let gameResultElem = document.getElementById("gameResult");
 
-//called when box is clicked
-function boxClicked(event){
-    
-    //change background color
-    event.target.style.background = colors[whoseTurn];
-    //remove event listener so it doesn't change color again
-    document.getElementById(event.target.id).removeEventListener("click",boxClicked);
-    //change whose turn it is
-    changeWhoseTurn();
-}
 
 //need to create 9 boxes with 3 on each row
 function createBoard(){
@@ -50,7 +42,79 @@ function createBoard(){
     changeWhoseTurn();
 }
 
-createBoard();
+//called when box is clicked
+function boxClicked(event){
+    //reset game result
+    gameResultElem.innerHTML="";
+    //get currentBox
+    let currBox = document.getElementById(event.target.id)
+    //remove event listener so it doesn't change color again
+    currBox.removeEventListener("click",boxClicked);
+    //change background color
+    currBox.setAttribute("style",`background:${colors[whoseTurn]}`);    
+    //change whose turn it is
+    changeWhoseTurn();
+    //increment clicks
+    clicks++;
+    //check for winner
+    checkForWin();
+}
+
+function checkForWin(){
+
+    let winner = "";
+    let allBoxes = Array.from(document.querySelectorAll(".box"));
+    //console.log(allBoxes);
+
+    //iterate through 8 combinations of winning numbers
+    for (let i=0;i<8 && winner == "";i++){
+        let boxesToCheck = [0,1,2];
+        //check columns for first 3 combinations
+        if (i<3){
+            boxesToCheck = boxesToCheck.map(num => (i*3)+num);
+        //check rows for second 3 combinations 
+        }else if (i>=3 && i <6)
+        {
+            boxesToCheck = boxesToCheck.map(num => (i-3)+(num*3));
+        //check first diagonal
+        }else if (i == 6){
+            boxesToCheck = [0,4,8];
+        //check last diagonal
+        }else{
+            boxesToCheck = [2,4,6];
+        }
+        //console.log(boxesToCheck);
+        
+        //filter for boxes that we want to check
+        let filteredBoxes = allBoxes.filter(box => boxesToCheck.indexOf(Number(box.id)) >= 0);
+        //console.log(filteredBoxes);
+
+        //check how many blue and red
+        let redCount = filteredBoxes.filter(box => box.style.background == colors[0]).length;
+        let blueCount = filteredBoxes.filter(box => box.style.background == colors[1]).length;
+
+        //console.log(i + " red "+redCount + " blue "+ blueCount);
+        if (redCount == 3){
+            gameResultElem.innerHTML="RED WINS!!!!";
+            gameResultElem.style.color = colors[0];
+            increRed();
+            clearBoard();
+        }
+        else if (blueCount == 3){
+            gameResultElem.innerHTML="BLUE WINS!!!!";
+            gameResultElem.style.color = colors[1];
+            increBlue();
+            clearBoard();
+        }
+    }
+
+    //if no winner and all boxes clicked, show tie
+    if (clicks == 9)
+    {
+        gameResultElem.innerHTML="Tie!";
+        //clearBoard();
+    }
+}
 
 function changeWhoseTurn(){
     whoseTurn = (whoseTurn+1)%2;
@@ -82,4 +146,9 @@ function clearBoard(){
         currBox.addEventListener("click",boxClicked);
         currBox.style.background = "#EFE7BC";
     }
+    //reset clicks
+    clicks = 0;
 }
+
+//create board
+createBoard();
