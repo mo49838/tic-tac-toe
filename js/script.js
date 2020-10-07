a72301b14557300fe8564bec9040b64515e613c1
Mark OBrien <obrienmark@johndeere.com>
2020-10-07 10:08:38 -0500

    Changing colors works

--- js/script.js
+++ js/script.js
@@ -3,16 +3,24 @@ console.log("javascript linked");
 //global variables
 let players = ["Red","Blue"];
 let colors = ["red","blue"];
-let whoseTurn = "X";
+let whoseTurn = 0;
 let redScore = 0;
 let blueScore = 0;
 
 //find dom objects
 let redScoreElem = document.getElementById("redScore");
 let blueScoreElem = document.getElementById("blueScore");
+let whoseTurnElem = document.getElementById("whoseTurn");
 
+//called when box is clicked
 function boxClicked(event){
     console.log(event);
+    //change background color
+    event.target.style.background = colors[whoseTurn];
+    //remove event listener so it doesn't change color again
+    document.getElementById(event.target.id).removeEventListener("click",boxClicked);
+    //change whose turn it is
+    changeWhoseTurn();
 }
 
 //need to create 9 boxes with 3 on each row
@@ -31,14 +39,24 @@ function createBoard(){
         for(let j=0;j<3;j++){
             let newBox = document.createElement("div");
             newBox.setAttribute("class","box");
+            //adding unique id for each box
+            newBox.setAttribute("id",(i*3)+j); 
             newBox.addEventListener("click",boxClicked);
             newRow.appendChild(newBox);
         }
     }
 
+    //used to display whose turn initially
+    changeWhoseTurn();
 }
 
 createBoard();
+
+function changeWhoseTurn(){
+    whoseTurn = (whoseTurn+1)%2;
+    whoseTurnElem.innerHTML=`${players[whoseTurn]}'s turn!`;
+    whoseTurnElem.style.color = colors[whoseTurn];
+}
 
 function resetScore(){
     redScore = 0;
